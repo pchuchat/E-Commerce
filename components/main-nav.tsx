d4c0f6cf4c79be28cd3b9fcb5a4ddebc021ee885
Phatchanon Chuchat <pchuchat@student.jyu.fi>
2023-08-22 17:39:01 +0300

    Settings

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -12,6 +12,11 @@ export function MainNav ({
     const params = useParams();
     const routes =[
         {
+            href:`/${params.storeId}`,
+            label:'Overview',
+            active: pathname === `/${params.storeId}`,
+        },
+        {
             href:`/${params.storeId}/settings`,
             label: 'Setting',
             active: pathname === `/${params.storeId}/settings`,
