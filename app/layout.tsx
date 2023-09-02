7d17393441abf32ab4982549a8a4cd0decf9baa1
Phatchanon Chuchat <pchuchat@student.jyu.fi>
2023-09-02 22:26:33 +0300

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ModalProvider } from '@/providers/modal-provider'
 import { ToasterProvider } from '@/providers/toast-provider'
+import { ThemeProvider } from '@/providers/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,9 +22,14 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
+      <ThemeProvider 
+       attribute="class" 
+       defaultTheme="system" 
+       enableSystem>
       <ToasterProvider />
       <ModalProvider />
       {children}
+      </ThemeProvider>
       </body>
     </html>
     </ClerkProvider>
